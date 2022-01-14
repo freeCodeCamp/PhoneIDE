e420062852ab979260af7799f79818b93ff9ea0f
Sem Bauke <46919888+Sembauke@users.noreply.github.com>
2022-01-14 10:15:54 +0100

    feat: file explorer (#2)
    * feat: file explorer
    
    * feat: model update
    
    * feat: view files and directories
    
    * fix: tree ui
    
    * fix: condense tree and add border

--- lib/controller/file_controller.dart
+++ lib/controller/file_controller.dart
@@ -1,11 +1,17 @@
 import 'dart:io';
-import 'dart:developer' as dev;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_code_editor/editor/file_explorer/file_explorer.dart';
+import 'package:flutter_code_editor/models/directory_model.dart';
+import 'package:flutter_code_editor/models/file_model.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileController {
-  // called when user has no projects directory
+  FileController({required this.fileExplorer});
 
-  static Future<String> initProjectsDirectory() async {
+  FileExplorer fileExplorer;
+
+  Future<String> initProjectsDirectory() async {
     Directory appDocDir = await getApplicationDocumentsDirectory();
 
     final Directory _appDocDirFolder = Directory('${appDocDir.path}/projects');
@@ -15,98 +21,29 @@ class FileController {
           await _appDocDirFolder.create(recursive: true);
       return _appDocDirNewFolder.path;
     }
+
     return _appDocDirFolder.path;
   }
 
-  // this will create a new project file when requested
-
-  static Future<String> initProject(String projectName) async {
-    String projectsDir = await initProjectsDirectory();
-
-    final Directory _projectFolder = Directory('$projectsDir/$projectName');
-
-    if (!await _projectFolder.exists()) {
-      final Directory _newProjectFolder =
-          await _projectFolder.create(recursive: true);
-      dev.log(_projectFolder.path);
-      return _newProjectFolder.path;
-    }
-
-    return _projectFolder.path;
-  }
-
-  // this will create a new file in a certain directory
-
-  static Future<String> createFile(
-      String projectName, String fileName, String ext) async {
-    String project = await initProject(projectName);
-
-    File file = File('$project/$fileName.$ext');
-
-    if (!await file.exists()) {
-      File file = await File('$project/$fileName.$ext').create(recursive: true);
-      return file.path;
-    }
-
-    return file.path;
-  }
-
-  // this will read a file in a certain directory the file name and directory
-  // will be provided by the project tree.
-
-  static Future<String> readFile() async {
-    // createFile also provides a path to it.
-
-    String filePath = await createFile('project', 'index', 'html');
-
-    final File file = File(filePath);
-
-    return await file.readAsString();
-  }
-
-  // this will write new values to a file in a certain directory when requested
-
-  static Future<void> writeFile(String code) async {
-    String filePath = await createFile('project', 'index', 'html');
-
-    final File file = File(filePath);
-
-    file.writeAsString(code);
-  }
-
-  static Future<List<String>> listProjects() async {
-    String path = await initProjectsDirectory();
-
+  Future<List<Widget>> listProjects(String path) async {
     final List<FileSystemEntity> projectPaths = Directory(path).listSync();
-
-    List<String> projects = [];
+    List<Widget> projects = [];
 
     for (int i = 0; i < projectPaths.length; i++) {
-      projects.add(projectPaths[i].path.split("/").last);
-    }
+      String path = projectPaths[i].path;
 
-    return projects;
-  }
-
-  static Future<Map<String, dynamic>> listProjectWithFiles() async {
-    String projectFolder = await initProjectsDirectory();
-
-    List<String> projects = await listProjects();
-
-    Map<String, dynamic> fileTree = {};
-
-    for (int i = 0; i < projects.length; i++) {
-      List<FileSystemEntity> filesInDir =
-          Directory('$projectFolder/${projects[i]}').listSync();
-
-      fileTree[projects[i]] = {};
-
-      for (int j = 0; j < filesInDir.length; j++) {
-        var fileName = filesInDir[j].path.split("/").last;
-        fileTree[projects[i]][fileName] = fileName;
+      if (await Directory(path).exists()) {
+        projects.add(DirectoryIDE(
+            fileExplorer: fileExplorer,
+            directoryName: path.split("/").last,
+            directoryPath: path,
+            directoryContent: await listProjects(path)));
+      } else {
+        projects.add(FileIDE(
+            fileName: path.split("/").last, filePath: path, fileContent: ""));
       }
     }
 
-    return fileTree;
+    return projects;
   }
 }
---
+++ lib/models/file_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FileIDE extends StatefulWidget {
+  const FileIDE(
+      {Key? key,
+      required this.fileName,
+      required this.filePath,
+      required this.fileContent})
+      : super(key: key);
+
+  final String fileName;
+  final String filePath;
+  final String fileContent;
+
+  @override
+  State<StatefulWidget> createState() => FileIDEState();
+}
+
+class FileIDEState extends State<FileIDE> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(left: BorderSide(width: 1, color: Colors.grey))),
+      child: ListTile(
+        leading: const Icon(Icons.insert_drive_file),
+        dense: true,
+        title: Text(widget.fileName),
+        onTap: () {},
+      ),
+    );
+  }
+}
