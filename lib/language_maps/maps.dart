53aee00be1309fb329bee8a3999bb618e8709003
SemBauke <semboot699@gmail.com>
2022-03-10 12:28:24 +0100

    fix: html map

--- lib/language_maps/maps.dart
+++ lib/language_maps/maps.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class RegexMaps {
   static Map<RegExp, TextStyle> htmlRegexMap = <RegExp, TextStyle>{
-    RegExp("<(“[^”]*”|'[^’]*’|[^'”>])*>"):
-        const TextStyle(color: Colors.yellow),
+    RegExp("</?.>"): const TextStyle(color: Colors.yellow),
+    RegExp(">"): const TextStyle(color: Colors.yellow),
+    RegExp("<."): const TextStyle(color: Colors.yellow),
     RegExp('".*"'): TextStyle(color: Colors.orange[300]),
-    RegExp('([A-Za-z\-]*)\='): TextStyle(color: Colors.green[300])
+    RegExp('([A-Za-z-]*)='): TextStyle(color: Colors.green[300]),
   };
 }
