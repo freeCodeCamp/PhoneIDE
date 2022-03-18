3cc527ad6ce18b0bde9ec638d5018e5731552fbd
SemBauke <semboot699@gmail.com>
2022-03-18 14:10:39 +0100

    fix: syntax highlighting for tags with multiple attributes

--- lib/language_maps/maps.dart
+++ lib/language_maps/maps.dart
@@ -4,7 +4,7 @@ class RegexMaps {
   static Map<RegExp, TextStyle> htmlRegexMap = <RegExp, TextStyle>{
     RegExp("<[^> \n]*"): const TextStyle(color: Colors.yellow),
     RegExp(">"): const TextStyle(color: Colors.yellow),
-    RegExp('".*"'): TextStyle(color: Colors.orange[300]),
+    RegExp('"[^"]*"'): TextStyle(color: Colors.orange[300]),
     RegExp('([A-Za-z]*)='): TextStyle(color: Colors.green[300]),
   };
 }
