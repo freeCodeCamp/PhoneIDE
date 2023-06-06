a5db73365d9e9f0b44bf3ccd2f498ea476efb62c
sembauke <semboot699@gmail.com>
2023-06-06 12:27:14 +0200

    fix: minimum sdk version

--- lib/editor/editor.dart
+++ lib/editor/editor.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:phone_ide/controller/custom_text_controller.dart';
